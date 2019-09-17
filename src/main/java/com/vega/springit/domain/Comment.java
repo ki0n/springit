f5a7ecedde89748730c83ff540d96145ee17591a
Kion <daniarkano@gmail.com>
2019-09-17 22:02:52 +0200

    Adding mappings from link->comment

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Objects;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Data
@@ -19,5 +19,7 @@ public class Comment {
     private String body;
 
     //Link
+    @ManyToOne
+    private Link link;
 
 }
