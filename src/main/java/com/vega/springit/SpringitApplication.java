f0be13fdd665ab4250881766acf0d493de3eba1b
Kion <daniarkano@gmail.com>
2019-09-22 20:01:50 +0200

    Implement List.html and Link class

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,70 +1,42 @@
 package com.vega.springit;
 
-import com.vega.springit.config.SpringitPropertiies;
-import com.vega.springit.domain.Comment;
-import com.vega.springit.domain.Link;
-import com.vega.springit.repository.CommentRepository;
-import com.vega.springit.repository.LinkRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import javafx.application.Application;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 
 @SpringBootApplication
-@EnableConfigurationProperties(SpringitPropertiies.class)
 @EnableJpaAuditing
 public class SpringitApplication {
 
-    @Autowired
-    private SpringitPropertiies springitPropertiies;
-
-    @Autowired
-    private ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
 
     }
 
-/*    @Bean
-    @Profile("dev")
-    CommandLineRunner runner(){
-        return args -> {
-          //System.out.println("Welcome message: " + springitPropertiies.getWelcomeMsg())//
-            // System.out.println("This is sometihn that we wold only do in dev");
-            System.out.println("Printing out all the bean names in the application context.");
-            System.out.println("-----------------------------------------------------------");
-            String[] beans = applicationContext.getBeanDefinitionNames();
-            Arrays.sort(beans);
-            for (String bean: beans){
-                //System.out.println(bean);
-            }
-        };
-    }*/
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(Application.class);
+    }
 
-    //@Bean
-    CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
-        return args -> {
-            Link link = new Link("Getting Started with Spring Bott 2","https://www.google.com");
-            linkRepository.save(link);
+    @Configuration
+    public class WebConfig implements WebMvcConfigurer{
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/static/**")
+            .addResourceLocations("classpath:/static/","classpath:/images/")
+            .setCachePeriod(0);
+        }
+    }
 
-            Comment comment = new Comment("This Spring Boot 2 link is awesomw",link);
-            commentRepository.save(comment);
-            link.addComment(comment);
-
-            System.out.println("We just inserted a link and a comment");
-
-            /*Link firstLink = linkRepository.findByTitle("Getting Started with Spring Boot 2");
-            System.out.println(firstLink.getTitle());*/
-        };
+    @Bean
+    PrettyTime prettyTime(){
+        return new PrettyTime();
     }
 }
--- src/main/java/com/vega/springit/controller/LinkController.java
+++ src/main/java/com/vega/springit/controller/LinkController.java
@@ -1,16 +1,13 @@
 package com.vega.springit.controller;
 
-import com.vega.springit.domain.Link;
 import com.vega.springit.repository.LinkRepository;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-import java.util.Optional;
 
-@RestController
-@RequestMapping("/links")
+@Controller
 public class LinkController {
-
 
     private LinkRepository linkRepository;
 
@@ -18,32 +15,9 @@ public class LinkController {
         this.linkRepository = linkRepository;
     }
 
-    // list
     @GetMapping("/")
-    public List<Link> list(){
-        return linkRepository.findAll();
-    }
-
-    // CRUD
-    @PostMapping("/create")
-    public Link create(@ModelAttribute Link link){
-        return linkRepository.save(link);
-    }
-
-    // /link/
-    @GetMapping("/{id}")
-    public Optional<Link> read(@PathVariable Long id){
-        return linkRepository.findById(id);
-    }
-
-    @PutMapping("/{id}")
-    public Link update(@PathVariable Long id, @ModelAttribute Link link){
-        //get the id
-        return linkRepository.save(link);
-    }
-
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id){
-        linkRepository.deleteById(id);
+    public String list(Model model){
+        model.addAttribute("links", linkRepository.findAll());
+        return "link/list";
     }
 }
