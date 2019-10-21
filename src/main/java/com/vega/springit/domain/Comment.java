09a15288ddb57ee599d2de007f2ce7eb5c31a41a
Kion <daniarkano@gmail.com>
2019-10-21 02:37:49 +0200

    initial configuration of comment content and database loader

--- src/main/java/com/vega/springit/domain/Comment.java
+++ src/main/java/com/vega/springit/domain/Comment.java
@@ -1,13 +1,18 @@
 package com.vega.springit.domain;
 
+import com.vega.springit.service.BeanUtil;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Entity
 @Data
@@ -17,6 +22,7 @@ public class Comment extends Auditable{
     @Id
     @GeneratedValue
     private Long id;
+
     @NonNull
     private String body;
 
@@ -28,5 +34,14 @@ public class Comment extends Auditable{
     public Comment(String body, Link link) {
         this.body = body;
         this.link = link;
+    }
+
+    public String getPrettyTime(){
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert){
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
